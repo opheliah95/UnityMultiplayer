36df80bd25155a355a1095f3844c4e629517436b
OphDev <opheliah95@gmail.com>
2020-08-04 00:31:53 +0100

    [Movement] Cleaned player movement function and added jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,66 +1,105 @@
-﻿using System.Collections;
+﻿using Mirror.Examples.Basic;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float runSpeed = 5f;
-    public float jumpHeight;
-    public float rotateSpeed;
+    [Header("Move Settings")]
+    public MoveSettings playerMoveSettings;
+    [Space]
 
-    [SerializeField]
-    Quaternion targetRotation; // next rotation
+    [Header("Physics Settings")]
+    public PhysSettings playerPhysSettings;
+    [Space]
+
     Rigidbody rb;
     [SerializeField]
     float forwardInput, turnInput;
     float inputDelay = 0.1f; // if input value under this value then we dont move
-    public Quaternion TargetRotation
+    bool isJumping = false;
+    public Vector3 velocity;
+    [System.Serializable]
+    public class MoveSettings
     {
-        get => targetRotation;
+        public float runSpeed = 5f;
+        public float jumpHeight = 5f;
+        public float raycastToGroundDistance = 0.3f; // how far away is it from ground
+        public Transform foot;
+        public LayerMask ground;
+      
     }
-
+    [System.Serializable]
+    public class PhysSettings
+    {
+        public float downAccelForce = 0.75f;
+    }
 
     // Start is called before the fdgirst frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        targetRotation = transform.rotation;
         forwardInput = turnInput = 0;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //Turn();
-    }
 
     private void FixedUpdate()
     {
-        StartMovement();
+        //StartMovement();
+
+        Jump();
+        //rb.velocity = velocity;
+        rb.velocity = transform.TransformDirection(velocity); // fixed the camera rotation bug
     }
 
-   public void UpdateMovementAxises(InputAction.CallbackContext context)
+    public void UpdateMovementAxises(InputAction.CallbackContext context)
     {
-        forwardInput = context.ReadValue<Vector2>().y;
-        turnInput = context.ReadValue<Vector2>().x;
+        velocity.z = context.ReadValue<Vector2>().y * playerMoveSettings.runSpeed;
+        velocity.x = context.ReadValue<Vector2>().x * playerMoveSettings.runSpeed;
     }
 
     private void StartMovement()
     {
-        rb.velocity = new Vector3(turnInput, 0, forwardInput) * runSpeed;
+        rb.velocity = new Vector3(turnInput, 0, forwardInput) * playerMoveSettings.runSpeed;
 
     }
-    
-    // turns the player
-    void Turn()
+
+
+    // player jump
+    public void OnJump(InputAction.CallbackContext context)
     {
-        if (Mathf.Abs(turnInput) > inputDelay)
+        isJumping = (!isJumping && isGrounded());
+    }
+
+    void Jump()
+    {
+        if (isJumping)
         {
-            //move
-            targetRotation *= Quaternion.AngleAxis(rotateSpeed * turnInput * Time.deltaTime, Vector3.up);
+            velocity.y = playerMoveSettings.jumpHeight;
         }
-        
-        //transform.rotation = targetRotation;
+        else if (isGrounded())
+        {
+            velocity.y = 0;
+        }
+        else
+        {
+            //decrease y velocity since it is falling
+            velocity.y -= playerPhysSettings.downAccelForce;
+        }
+
+    }
+
+    // check if player is grounded
+    bool isGrounded()
+    {
+        return Physics.Raycast(playerMoveSettings.foot.position, Vector3.down, playerMoveSettings.raycastToGroundDistance, playerMoveSettings.ground);
+    }
+
+    // check distance to ground
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(playerMoveSettings.foot.position,  playerMoveSettings.raycastToGroundDistance);
     }
 }
